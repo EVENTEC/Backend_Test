52b4001fdba6d28ddc7ac12231aebde00492a11f
Alejandro Zárate <102533262+AlejandroZarateGamarra@users.noreply.github.com>
2024-06-21 21:41:18 -0500

    Actualización de dependencias

--- myevent/src/main/java/com/eventec/myevent/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ myevent/src/main/java/com/eventec/myevent/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -21,14 +21,14 @@ public class OpenApiConfiguration {
         var openApi = new OpenAPI();
         openApi
                 .info(new Info()
-                        .title("ACME Learning Platform API - UPC")
-                        .description("ACME Learning Platform application REST API documentation.")
+                        .title("MyEvent Platform API - UPC")
+                        .description("MyEvent Platform application REST API documentation.")
                         .version("v1.0.0")
                         .license(new License().name("Apache 2.0")
                                 .url("https://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("ACME Learning Platform wiki Documentation")
-                        .url("https://acme-learning-platform.wiki.github.io/docs"));
+                        .description("MyEvent Platform wiki Documentation"));
+                        //.url("https://acme-learning-platform.wiki.github.io/docs"));
         return openApi;
     }
 }
